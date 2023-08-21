c72569878e969f08a09ea06b33055d79c8a2749b
Pham <peter.pham@cgi.com>
2023-08-21 12:04:23 +0200

    latest change

--- src/main/java/net/java/todoapi/cores/config/WebConfig.java
+++ src/main/java/net/java/todoapi/cores/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5173/")
+                .allowedOrigins("http://127.0.0.1:5173")
                 .allowedMethods("GET", "POST","PUT", "DELETE")
                 .allowCredentials(true);
     }
